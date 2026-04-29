acdd0798517e46cfba9494b14a7f88d8ef224e3b
maksim-luzin <333maximax333@gmail.com>
2026-04-29 12:59:50 +0300

    fix/get-top-5-counterparties Fix data for test

--- src/feature/top-5-counterparties-by-spending-volume/lib/__tests__/get-top-5-counterparties-by-spending-volume.test.ts
+++ src/feature/top-5-counterparties-by-spending-volume/lib/__tests__/get-top-5-counterparties-by-spending-volume.test.ts
@@ -6,7 +6,7 @@ import dataForTestJSON from './data-for-test.json';
 describe('Transaction total info test', () => {
   it('success', () => {
     const successResult = [
-      ['ТОВ 'Ета Білд'', -23800],
+      ['ТОВ "Ета Білд"', -23800],
       ['ФОП Шевченко В.П.', -9000],
       ['Rozetka', -3200],
       ['Епіцентр', -2850],
@@ -22,7 +22,7 @@ describe('Transaction total info test', () => {
     const failureResult = [
       ['Rozetka', -3200],
       ['Епіцентр', -2850],
-      ['ТОВ 'Ета Білд'', -23800],
+      ['ТОВ "Ета Білд"', -23800],
       ['ФОП Шевченко В.П.', -9000],
       ['ПриватБанк', -630],
     ];
